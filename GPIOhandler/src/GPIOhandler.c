880e984143e7314b75e0b772754bc187c6d1d666
Johannes Strasser <johannes.strasser@strasys.at>
2014-11-30 10:19:05 +0100

    new main init in main

---
+++ GPIOhandler/src/GPIOhandler.c
@@ -0,0 +1,22 @@
+/*
+ * RTChandler.c
+ *
+ *  Created on: 08.11.2014
+ *      Author: Johannes Strasser
+ *
+ *This program is supposed to be called from the server
+ *to set and read the on board RTC.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "/home/johannes/git/firmware/src/functions/GPIO.h"
+
+int main(int argc, char *argv[], char *env[]){
+
+
+
+	return 0;
+}
---
+++ webinterface/setgetTime.php
@@ -0,0 +1,36 @@
+<?php
+	$td;
+	$ausgabe;
+	$arr;
+	unset($td);
+	unset($ausgabe);
+	unset($arr);
+	$td = 0;
+	$td = $_POST["td"];
+	$hh = $_POST["hh"];
+	$mm = $_POST["mm"];
+	$ss = $_POST["ss"];
+	$Day = $_POST["Day"];
+	$Month = $_POST["Month"];
+	$Year = $_POST["Year"];
+	$t = "t";
+	$d = "d";
+	
+	if ($td == $t){
+		exec("./cgi-bin/RTChandler s t $hh $mm $ss", &$ausgabe);
+		}
+	elseif ($td == $d){
+		exec("./cgi-bin/RTChandler s d $Day $Month $Year", &$ausgabe);
+		}
+	elseif ($td == 0) {
+	exec("./cgi-bin/RTChandler", &$ausgabe);	
+	}
+	
+	$arr = array(	'Day' => $ausgabe[0],
+					'Month' => $ausgabe[1],
+					'Year' => $ausgabe[2],
+					'hh' => $ausgabe[3],
+					'mm' => $ausgabe[4],
+					'ss' => $ausgabe[5]);
+	echo json_encode($arr);
+?>

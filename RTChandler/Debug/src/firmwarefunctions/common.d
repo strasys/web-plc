48a3103e434c63941d6bf28d6bbccfe37bb98879
Johannes Strasser <johannes.strasser@strasys.at>
2014-11-30 11:37:58 +0100

    new RTChandler

---
+++ RTChandler/Debug/src/firmwarefunctions/common.d
@@ -0,0 +1,11 @@
+src/firmwarefunctions/common.d: \
+ /home/johannes/git/firmware/src/functions/common.c \
+ /home/johannes/git/firmware/src/functions/common.h \
+ /home/johannes/git/firmware/src/functions/I2C-handler.h \
+ /home/johannes/git/firmware/src/functions/RTC_MCP7940N.h
+
+/home/johannes/git/firmware/src/functions/common.h:
+
+/home/johannes/git/firmware/src/functions/I2C-handler.h:
+
+/home/johannes/git/firmware/src/functions/RTC_MCP7940N.h:

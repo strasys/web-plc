ef04508e5e172ad36ff7500079cb631634703835
Franz.Mauch <Franz.Mauch@t-online.de>
2014-12-04 21:30:54 +0100

    RTC-Handler format

--- RTChandler/src/RTChandler.c
+++ RTChandler/src/RTChandler.c
@@ -36,13 +36,18 @@ int main(int argc, char *argv[], char *env[]){
 			RTC_set_year(atoi(argv[5]));
 		}
 
-	printf("%d\n", RTC_get_day());
-	printf("%d\n", RTC_get_month());
-	printf("%d\n", RTC_get_year());
-	printf("%d\n", RTC_get_hours());
-	printf("%d\n", RTC_get_minutes());
-	printf("%d\n", RTC_get_seconds());
-
+//	if ((setget == 'g')) {
+	if (timedate == 'f') {
+		printf("%4.4d%2.2d%2.2d%2.2d%2.2d%2.2d", RTC_get_year(),RTC_get_month(),RTC_get_day()
+				                               , RTC_get_hours(),RTC_get_minutes(),RTC_get_seconds());
+	} else {
+	printf("%2.2d\n", RTC_get_day());
+	printf("%2.2d\n", RTC_get_month());
+	printf("%4.4d\n", RTC_get_year());
+	printf("%2.2d\n", RTC_get_hours());
+	printf("%2.2d\n", RTC_get_minutes());
+	printf("%2.2d\n", RTC_get_seconds());
+	}
 	return 0;
 }
 

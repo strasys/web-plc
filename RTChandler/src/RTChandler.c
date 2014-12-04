b79fa4a8b94f26bbd10091949f9dd4baa1424025
Franz.Mauch <Franz.Mauch@t-online.de>
2014-12-04 22:14:11 +0100

    RTC-Handler format2

--- RTChandler/src/RTChandler.c
+++ RTChandler/src/RTChandler.c
@@ -6,7 +6,7 @@
  *
  *This program is supposed to be called from the server
  *to set and read the on board RTC.
-*/
+ */
 
 #include <stdio.h>
 #include <string.h>
@@ -15,41 +15,52 @@
 #include "/home/johannes/git/firmware/src/functions/I2C-handler.h"
 #include "/home/johannes/git/firmware/src/functions/RTC_MCP7940N.h"
 
-
-int main(int argc, char *argv[], char *env[]){
+int main(int argc, char *argv[], char *env[]) {
 	char setget, timedate;
+	char[13] datetime;
 
-	if (argv[1] != 0){
-	sscanf(argv[1], "%c", &setget);
-	sscanf(argv[2], "%c", &timedate);
+	if (argv[1] != 0) {
+		sscanf(argv[1], "%c", &setget);
+		sscanf(argv[2], "%c", &timedate);
 	}
 
-	if ((setget == 's')&&(timedate == 't')){
-		RTC_set_hours(atoi(argv[3]));
-		RTC_set_minutes(atoi(argv[4]));
-		RTC_set_seconds(atoi(argv[5]));
-	}
+	if (setget == 's') {
+		char command[128];
+		if (timedate == 't') {
+			RTC_set_hours(atoi(argv[3]));
+			RTC_set_minutes(atoi(argv[4]));
+			RTC_set_seconds(atoi(argv[5]));
+		}
 
-	if	((setget == 's')&&(timedate == 'd')){
+		if (timedate == 'd') {
 			RTC_set_day(atoi(argv[3]));
 			RTC_set_month(atoi(argv[4]));
 			RTC_set_year(atoi(argv[5]));
 		}
+		getFormatForDate(dateTime);
+		sprintf(command,"date -u %s",dateTime);
+		system(command);
+	}
 
 //	if ((setget == 'g')) {
 	if (timedate == 'f') {
-		// formate for date -u
-		printf("%2.2d%2.2d%2.2d%2.2d%4.4d", RTC_get_month(),RTC_get_day()
-				                          , RTC_get_hours(),RTC_get_minutes()
-										  , RTC_get_year());
+		getFormatForDate(dateTime);
+		printf("%s", dateTime);
 	} else {
-	printf("%2.2d\n", RTC_get_day());
-	printf("%2.2d\n", RTC_get_month());
-	printf("%4.4d\n", RTC_get_year());
-	printf("%2.2d\n", RTC_get_hours());
-	printf("%2.2d\n", RTC_get_minutes());
-	printf("%2.2d\n", RTC_get_seconds());
+		printf("%2.2d\n", RTC_get_day());
+		printf("%2.2d\n", RTC_get_month());
+		printf("%4.4d\n", RTC_get_year());
+		printf("%2.2d\n", RTC_get_hours());
+		printf("%2.2d\n", RTC_get_minutes());
+		printf("%2.2d\n", RTC_get_seconds());
 	}
 	return 0;
+}
+
+void getFormatForDate(char * pDateTime) {
+	// formats for date -u
+	// date --universal $(/www/pages/cgi-bin/RTChandler g f)
+	sprintf(pDateTime, "%2.2d%2.2d%2.2d%2.2d%4.4d", RTC_get_month(),
+			RTC_get_day(), RTC_get_hours(), RTC_get_minutes(), RTC_get_year());
 }
 

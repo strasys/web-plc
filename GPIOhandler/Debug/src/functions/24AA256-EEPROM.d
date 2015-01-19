9645ab9c71a574e3aacc85e16073e4f25fdcadec
Johannes Strasser <johannes.strasser@strasys.at>
2015-01-19 20:06:02 +0100

    Debug GPIO webinterface

---
+++ GPIOhandler/Debug/src/functions/24AA256-EEPROM.d
@@ -0,0 +1,8 @@
+src/functions/24AA256-EEPROM.d: \
+ /home/johannes/git/firmware/src/functions/24AA256-EEPROM.c \
+ /home/johannes/git/firmware/src/functions/I2C-handler.h \
+ /home/johannes/git/firmware/src/functions/24AA256-EEPROM.h
+
+/home/johannes/git/firmware/src/functions/I2C-handler.h:
+
+/home/johannes/git/firmware/src/functions/24AA256-EEPROM.h:

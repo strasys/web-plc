f9825f4f3ebde31ae16df12c9fb5ce8b49d99c14
Johannes Strasser <johannes.strasser@strasys.at>
2015-04-02 00:46:26 +0200

    AINOUThandler new

---
+++ src/AINOUThandler/AINOUThandler.c
@@ -0,0 +1,52 @@
+/*
+ * AINOUThandler.c
+ *
+ *  Created on: 01.04.2015
+ *      Author: Johannes Strasser
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <AOUT_LTC2635.h>
+#include <AIN-handler.h>
+
+
+
+
+int main(int argc, char *argv[], char *env[]){
+	int channel, AINvalue, AOUTvalue;
+	char setget;
+	char InOut;
+
+	if (argv[1] != 0){
+	sscanf(argv[1], "%c", &setget);
+	}
+
+	if (setget == 'g'){
+		sscanf(argv[2], "%c", &InOut);
+		channel = atoi(argv[3]);
+		if (InOut == 'I'){
+			AINvalue = get_iio_value_n(channel);
+			printf("%i", AINvalue);
+		}
+		else if (InOut == 'O'){
+			AOUTvalue = AOUT_get_value_DACn(channel);
+			printf("%i", AOUTvalue);
+		}
+	}
+
+	if (setget == 's'){
+		sscanf(argv[2], "%c", &InOut);
+		channel = atoi(argv[3]);
+		AOUTvalue = atoi(argv[4]);
+		if (InOut == 'O'){
+			AOUT_set_value_DACn(channel, AOUTvalue);
+		}
+
+	}
+
+return 0;
+
+}

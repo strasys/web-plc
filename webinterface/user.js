26fefffd14b923901b798c98c4921914b75fb879
Johannes Strasser <johannes.strasser@strasys.at>
2015-06-12 00:38:39 +0200

    user management update

--- webinterface/user.js
+++ webinterface/user.js
@@ -7,6 +7,32 @@
  * www.strasys.at
  */
 
+ 
+function setgetuser(setget, url, cfunc, senddata){
+	xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = cfunc;
+	xhttp.open(setget,url,true);
+	xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+	xhttp.send(senddata);
+}
+
+ function setgetUserPassword(callback1){
+		setgetuser("post","user.php",function()
+			{
+				if (xhttp.readyState==4 && xhttp.status==200)
+				{
+				var getUserInfo = JSON.parse(xhttp.responseText); 
+				
+				userInfo = [(getAIn.INvalue1),
+				            (getAIn.INvalue2)
+				               ];
+					if (callback1){
+						callback1();
+					}
+				}
+			},"setgetAnalog=g&InOut=I");		
+}
+  
 //load functions at webpage opening
 function startatLoad(){
 	loadNavbar();

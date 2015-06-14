3d0459dfcde19a441a6b3f307e14cd1d586ac728
Johannes Strasser <johannes.strasser@strasys.at>
2015-06-14 23:25:53 +0200

    login with session added

--- webinterface/login.php
+++ webinterface/login.php
@@ -1,4 +1,29 @@
 <?php
+session_start(); 
+$username = $_POST["username"]; 
+$password = $_POST["password"]; 
+$passwordEncrypt = md5($passwort); 
+$log=0; 
 
+$userfile = fopen ("user.txt","r"); 
+while (!feof($userfile)) 
+   { 
+   $line = fgets($userdatei,500); 
+   $userdata = explode("|", $line); 
+
+   if ($userdata[0]==$username and $passwordEncrypt==trim($userdata[1])) 
+      { 
+      $_SESSION['username'] = $username; 
+        echo "Login war erfolgreich. <a href=\"geheim.php\">Geschützer Bereich</a>"; 
+      $log = 1; 
+      } 
+   } 
+fclose($userdatei); 
+
+if ($log==0) 
+   { 
+   echo "Zugriff verweigert <a href=\"login.html\">Zurück</a>"; 
+   } 
+?>
 
 ?>

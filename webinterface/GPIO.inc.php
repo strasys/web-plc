e6b525432fd8f9149474fe723fbd728519576ce9
Johannes Strasser <johannes.strasser@strasys.at>
2015-10-07 23:37:05 +0200

    class GPIO added

---
+++ webinterface/GPIO.inc.php
@@ -0,0 +1,53 @@
+<?php
+/*
+	class GPIO
+	
+	Johannes Strasser
+	07.10.2015
+	www.strasys.at
+*/
+class GPIO
+{
+	private $OUT1;
+	private $OUT2;
+	private $OUT3;
+	private $OUT4;
+	private $OUT5;
+	private $OUT6;
+	private $OUT7;
+	private $OUT8;
+	
+	/*Generate innitial Digital Output Settings.*/
+/*	function __construct($out)
+	{
+		$this->$OUT1 = $out[0];
+		$this->$OUT2 = $out[1];
+		$this->$OUT3 = $out[2];
+		$this->$OUT4 = $out[3];
+		$this->$OUT5 = $out[4];
+		$this->$OUT6 = $out[5];
+		$this->$OUT7 = $out[6];
+		$this->$OUT8 = $out[7];
+	}
+*/
+
+	/*set the digital output (0 = off, 1 = on)  */
+	function setOut($out[0], $out[1], $out[2], $out[3], $out[4], $out[5], $out[6], $out[7])
+	{
+		for ($i = 0; $i<8; $i++)
+		{
+			exec(" /usr/lib/cgi-bin/GPIOhandler s $i $out");
+		}
+	}
+	
+	function getOut()
+	{
+	
+	}
+	
+	function getIn()
+	{
+	
+	}
+}
+?>

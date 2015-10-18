55ddd92948dbd9cb03d4922403e6a77dea7299bf
Johannes Strasser <johannes.strasser@strasys.at>
2015-10-18 23:28:26 +0200

    pushButtonSensin.c debuged

--- src/pushButtonSensing/pushButtonSensing.c
+++ src/pushButtonSensing/pushButtonSensing.c
@@ -13,16 +13,31 @@
 #include <errno.h>
 #include "GPIO.h"
 
-int getboolRunStop(char charRunStop){
+int getboolRunStop(char *charRunStop){
 	int boolRunStop;
-	switch (boolRunStop){
-	case "stop":
-		boolRunStop = 0;
-		break;
-	case "run":
-		boolRunStop = 1;
-		break;
+	char charStop[4], charRun[3];
+	//char RunStop[5];
+	//printf("Übergabe an getboolRunStop = %s\n",charRunStop);
+	strcpy(charStop,"stop");
+	strcpy(charRun, "run");
+	//printf("%s\n",charRunStop);
+	//printf("%s\n",charStop);
+	//printf("%s\n",charRun);
 
+	if (strcmp(charRunStop,charStop) == 0)
+	{
+		boolRunStop = 0;
+	}
+	else if (strcmp(charRunStop,charRun) == 0)
+	{
+		boolRunStop = 1;
+	}
+	else
+	{
+		fprintf(stderr, "String comparison does not match: %s\n", strerror( errno ));
+		return EXIT_FAILURE;
+	}
+	//printf("boolRunStop = %i\n", boolRunStop);
 	return boolRunStop;
 }
 
@@ -36,16 +51,16 @@ char getcharRunStop(int boolRunStop){
 		case 1:
 			sprintf(charRunStop, "run");
 			break;
-
-		return charRunStop;
 	}
+		return *charRunStop;
+}
 
 int getRunStopStatus() {
 	FILE *f = NULL;
 	char DIR_getRunStopStatus[255] = {};
 	char charRunStop[5] = {};
 	char fopenModus[2] = {};
-	int flag = 0;
+	int flag = 0, RunStopStatus;
 
 	sprintf(DIR_getRunStopStatus, "/tmp/pushButtonSensingRunStop.txt");
 
@@ -59,25 +74,31 @@ int getRunStopStatus() {
 
 	if (flag == 0){
 		f = fopen(DIR_getRunStopStatus, fopenModus);
-		fread(charRunStop,sizeof(charRunStop),f);
+		fread(charRunStop,sizeof(charRunStop),sizeof(charRunStop),f);
+		sprintf(charRunStop,"%s%s",charRunStop,"\0");
+		//printf("Info direkt nach fread: %s\n",charRunStop);
 		fclose(f);
-		return getboolRunStop(charRunStop);
+
+		RunStopStatus = getboolRunStop(charRunStop);
 	}
 	else if(flag == 1){
 		f = fopen(DIR_getRunStopStatus, fopenModus);
 		sprintf(charRunStop, "stop");
-		fwrite(charRunStop,5,f);
+		fwrite(charRunStop,5,5,f);
 		fclose(f);
-		return 0;
+		RunStopStatus = 0;
 	}
+	return RunStopStatus;
 }
 
-void writeDigiInStatus(int boolDigiInStatus[][]) {
+void writeDigiInStatus(char *DigiInStatus) {
 	FILE *f = NULL;
 	char DIR_writeDigiInStatus[255] = {};
 	char InStatus[7] = {};
 	char fopenModus[2] = {};
+	char buffer1[4] = {}; //buffer2[5]={};
 	int i = 0;
+	printf("writeDigiInStatus erreicht\n");
 
 	sprintf(DIR_writeDigiInStatus, "/tmp/pushButtonSensingDigiInStatus.txt");
 
@@ -87,9 +108,11 @@ void writeDigiInStatus(int boolDigiInStatus[][]) {
 		sprintf(fopenModus, "w");
 	}
 
+	sprintf(buffer1,"%s",DigiInStatus);
+	printf("buffer1 = %s\n", buffer1);
 	f = fopen(DIR_writeDigiInStatus, fopenModus);
 	for (i=0; i<4; i++){
-		sprintf(InStatus,"IN%i:%i\n",boolDigiInStatus[i][1]);
+		sprintf(InStatus,"IN%i:%c\n",i,buffer1[i]);
 		fprintf(f,"%s",InStatus);
 	}
 	fclose(f);
@@ -97,8 +120,11 @@ void writeDigiInStatus(int boolDigiInStatus[][]) {
 }
 
 int main(int argc, char *argv[], char *env[]){
-	int SensingInput[4] = {0,0,0,0};
-	int i = 0;
+	char SensingInput[3] = {};
+	char InputStatusNew[4] = {'1','1','1','1'};
+	char InputStatusOld[4]={'1','1','1','1'};
+	char InputStatus[4]={'1','1','1','1'};
+	int i = 0, runstop = 1, InNumber, flagWriteDigiInStatus=0, sensingCycleTime;
 
 /*
  * Get arguments what Inputs should be considered
@@ -106,12 +132,16 @@ int main(int argc, char *argv[], char *env[]){
  * 0 = sensing no
  * 1 = sensing yes
  */
-	if (argv[1] && argv[2] && argv[3] && argv[4]!=0){
-		for (i=1;i<5;i++){
-		sscanf(argv[i],"%i",&SensingInput[i-1]);
-		if (SensingInput[i-1] != (0 || 1)){
-			fprintf(stderr, "Wrong argument value: %s\n", strerror( errno ));
-			return EXIT_FAILURE;
+	if ((argv[1] && argv[2] && argv[3] && argv[4])!=0){
+		for (i=1;i<5;i++)
+		{
+			sscanf(argv[i],"%c",&SensingInput[i-1]);
+			printf("Wert SensingInput = %c\n",SensingInput[i-1]);
+
+			if (((SensingInput[i-1]) != '0')&&(SensingInput[i-1] != '1'))
+			{
+				fprintf(stderr, "Wrong argument value: %s\n", strerror( errno ));
+				return EXIT_FAILURE;
 			}
 		}
 	}
@@ -121,9 +151,68 @@ int main(int argc, char *argv[], char *env[]){
 		return EXIT_FAILURE;
 	}
 
-while()
-	gpio_get_value(IN_OUT[i][0]);
+	if (argv[5]!=0)
+	{
+		sensingCycleTime = atoi(argv[5])*1000; //sensing in xx ms
+	}
+	else
+	{
+		sensingCycleTime = 80000; //standard sensing time if nothing is set
+	}
 
-usleep(50000);
+	while(runstop == 1)
+	{
+
+		/*
+		 * get status of input channel
+		 * 1 = no high signal / 0 = high signal on input
+		 * N = Is the marker that this channel is not considered for sensing.
+		 */
+		for (i=0;i<sizeof(SensingInput)+1;i++)
+		{
+			if (SensingInput[i] == '1')
+			{
+				InNumber = i + 8; //8 is the offset to read only IN channels.
+
+				InputStatusNew[i] =	gpio_get_value(IN_OUT[InNumber][0])+'0';
+
+			}
+			else
+			{
+				//Mark an Input which is not considered for pushButtonSensing.
+				InputStatus[i] = 'N';
+			}
+			//It is only interresting to sens the 0 value.
+			if ((InputStatusNew[i] == '0') && (InputStatusOld[i] == '1') && (InputStatus[i] != 'N'))
+			{
+				//change IN switch Status
+				if (InputStatus[i] == '1')
+					{InputStatus[i] = '0';}
+				else if (InputStatus[i] == '0')
+					{InputStatus[i] = '1';}
+
+				//set write flag
+				flagWriteDigiInStatus = -1;
+			}
+			//remember status to sense status change
+				InputStatusOld[i] = InputStatusNew[i];
+		}
+		printf("InputStatus = %c\n",InputStatus[0]);
+		printf("InputStatus = %c\n",InputStatus[1]);
+		printf("InputStatusNew = %c\n",InputStatusNew[0]);
+		printf("InputStatusNew = %c\n",InputStatusNew[1]);
+
+		if (flagWriteDigiInStatus == -1)
+		{
+			writeDigiInStatus(InputStatus);
+		}
+		usleep(sensingCycleTime);
+		//Wihout the getRunStopSatus() it is not possible to control the pushButtonSensing function.
+		runstop = getRunStopStatus();
+		//set variables to initial status
+		flagWriteDigiInStatus = 0;
+	}
+
+	return 0;
 }
 
